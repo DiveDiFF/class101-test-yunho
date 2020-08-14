41d18206ef9461dd29d316a299227a35746944d0
YUNHO <silveruno123@gmail.com>
2020-08-15 02:54:23 +0900

    Add list of page-cart

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -2,30 +2,78 @@ import React from 'react';
 
 import {fetchData} from '../actions';
 import {Title} from '../components/title';
+import {MaterialIcon} from '../components/widgets';
 
 import {withStyles} from '@material-ui/core/styles';
-import {Paper} from '@material-ui/core';
+import {
+  Paper, List, ListItem, ListItemIcon, Checkbox,
+  ListItemText, Typography, ListItemSecondaryAction, IconButton
+} from '@material-ui/core';
 
 const styles = theme => ({
   root: {
     marginTop: '68px',
     height: '90vh',
   },
+  list: {
+    padding: '0px 80px'
+  },
+  title: {
+    textAlign: 'center',
+    flex: '1 0 28%',
+  },
+  img: {
+    maxWidth: 200,
+  },
 });
 class Cart extends React.Component {
   state = {
-    cart : [],
+    cart: [],
+    checkedItem: [],
   }
 
   render() {
-    const {classes} = this.props
+    const {classes} = this.props;
+    const {cart, checkedItem} = this.state;
 
     return(
       <Paper className={classes.root} elevation={0}>
         <Title title="장바구니" />
-
+        <List className={classes.list}>
+          {cart && cart.map((item, index) =>
+            <ListItem divider button
+              key={item.id || index + Math.random()}>
+              <ListItemIcon>
+                <Checkbox edge="start" checked={checkedItem.indexOf(item.id) !== -1}/>
+              </ListItemIcon>
+              <img src={item.coverImage} className={classes.img}/>
+              <ListItemText className={classes.title}>
+                <Typography variant="body2">{item.title}</Typography>
+              </ListItemText>
+              <ListItemText>
+                <Typography variant="body2">{`월 ${item.price.toLocaleString()}원`}</Typography>
+              </ListItemText>
+              <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="delete">
+                <MaterialIcon icon="delete" title="삭제"/>
+              </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          )}
+        </List>
       </Paper>
     );
+  }
+
+  componentDidMount() {
+    fetchData(`/cart`, {method: 'GET'})
+      .then(cart => {
+        const checkedItem = [];
+        cart.forEach(item => {
+          return checkedItem.push(item.id);
+        });
+        this.setState({...this.state, cart, checkedItem});
+      }).catch(error => window.alert(`[오류] 상품 정보를 불러올 수 없습니다. ${error}`));
   }
 }
 
